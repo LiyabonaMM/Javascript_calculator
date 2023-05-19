c6fb9f46c475f863e6274d17e2446f474f628e2d
Liyabona Mihlali Mxhalisa <106662929+LiyabonaMM@users.noreply.github.com>
2023-05-19 23:09:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-/*let sum = '';
-
-function appendValue(value) {
+//let sum = '';: This line declares a variable sum and initializes it with an empty string. 
+//This variable will store the arithmetic expression entered by the user.
+let sum = '';
+const output = document.getElementById('output');
+//This line selects the HTML element with the ID output using the getElementById function. 
+//The output constant refers to the input element where the calculation result will be displayed.
+function appendToOutput(value) {
   sum += value;
-  document.getElementById('output').value = sum;
+  output.value = sum;
 }
+//This is a function that is called when a number or operator button is clicked. 
+//It takes a value parameter representing the button value
 
-function appendOperator(operator) {
-  if (sum !== '' && !isNaN(sum[sum.length - 1])) {
-    sum += operator;
-    document.getElementById('output').value = sum;
+function calculateSum() {
+  try {
+    const result = eval(sum);
+    output.value = result;
+    sum = result.toString();
+  } catch (error) {
+    output.value = 'Error';
   }
 }
-
-function evaluateSum() {
-  if (sum !== '' && !isNaN(sum[sum.length - 1])) {
-    try {
-      const result = eval(sum);
-      document.getElementById('output').value = result;
-      sum = result.toString();
-    } catch (error) {
-      document.getElementById('output').value = 'Error';
-      sum = '';
-    }
-  }
-}
+//when the equal button is pressed it must calculate that expression stored in th 'sum' using the eval()
+//results arre assigned to 'result' constant
+//f an error occurs during evaluation, the output element displays an "Error" message.
 
 function clearSum() {
   sum = '';
-  document.getElementById('output').value = '';
+  output.value = '';
 }
-*/
